6b273b9739ee261a5416adbd7913546862b1cb1c
Bryan Hatcher <bryanhatcher34@gmail.com>
2022-11-01 12:33:20 -0400

    Made data more persistant

--- client/src/Context/UserContext.jsx
+++ client/src/Context/UserContext.jsx
@@ -1,9 +1,19 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const UserContext = createContext(null);
 
 export const UserContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    if(localStorage.getItem("User")){
+      setCurrentUser(JSON.parse(localStorage.getItem("User")))
+    }
+    else{
+      setCurrentUser({})
+    }
+  },[])
+  
 
   return (
     <UserContext.Provider value={{ currentUser, setCurrentUser }}>
